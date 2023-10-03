600421abe08a90653ceca6d3b31c3265fd80c855
Yahya <yahyaoulha0@gmail.com>
2023-10-03 21:35:46 +0100

    worked

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:-62], end='')
-print(str[107:-18], str[0:-123], "\n", end='')
+print(str[107:-17] + str[0:-123] + "\n", end='')
