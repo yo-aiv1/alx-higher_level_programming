05f07d73c5550dbcfdcc768fff8963174b3c0b63
Yahya <yahyaoulha0@gmail.com>
2023-10-04 11:19:05 +0100

    worked

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def check(num):
-    if num in range(121, 98, -1):
-        return 32
-    else:
-        return 0
 for i in range(122, 96, -1):
     if i % 2 == 0:
         print(chr(i), end='')
