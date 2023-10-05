ca88bfbfc3de10d6ef76bdcf9ee3cb7678e9f940
Yahya <yahyaoulha0@gmail.com>
2023-10-06 00:32:40 +0100

    worked

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,21 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import sub, mul, div, add
+    first = int(sys.argv[1])
+    second = int(sys.argv[3])
+    result = add(int(sys.argv[1]), int(sys.argv[3]))
     if len(sys.argv) > 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     else:
         if '+' in sys.argv[2]:
-            print("{} + {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), add(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} + {} = {}".format(first, second, result))
         elif '-' in sys.argv[2]:
-            print("{} - {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), sub(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} - {} = {}".format(first, second, result))
         elif '*' in sys.argv[2]:
-            print("{} * {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), mul(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} * {} = {}".format(first, second, result))
         elif '/' in sys.argv[2]:
-            print("{} / {} = {}".format(int(sys.argv[1]), int(sys.argv[3]), div(int(sys.argv[1]), int(sys.argv[3]))))
+            print("{} / {} = {}".format(first, second, result))
         else:
             print('Unknown operator. Available operators: +, -, * and /')
             exit(1)
