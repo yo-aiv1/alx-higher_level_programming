2f30fd8dc70f95f2e45d735f5221e3012b088c3d
Yahya <yahyaoulha0@gmail.com>
2023-10-05 11:13:48 +0100

    worked

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(a,"{}".format('+'), b, "{}".format('='),add(a, b))
+    print(a,"{}".format('-'), b, "{}".format('='),sub(a, b))
+    print(a,"{}".format('*'), b, "{}".format('='),mul(a, b))
+    print(a,"{}".format('/'), b, "{}".format('='),div(a, b))
