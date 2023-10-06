deef6539a5532374990a99ad9e0ee9cff36c188e
Yahya <yahyaoulha0@gmail.com>
2023-10-06 23:12:43 +0100

    fixed styling

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(a,"{}".format('+'), b, "{}".format('='),add(a, b))
-    print(a,"{}".format('-'), b, "{}".format('='),sub(a, b))
-    print(a,"{}".format('*'), b, "{}".format('='),mul(a, b))
-    print(a,"{}".format('/'), b, "{}".format('='),div(a, b))
+    print(a, "{}".format('+'), b, "{}".format('='), add(a, b))
+    print(a, "{}".format('-'), b, "{}".format('='), sub(a, b))
+    print(a, "{}".format('*'), b, "{}".format('='), mul(a, b))
+    print(a, "{}".format('/'), b, "{}".format('='), div(a, b))
