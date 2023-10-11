3c98031e1e49945e8e8006582553e55610f3df2c
Yahya <yahyaoulha0@gmail.com>
2023-10-11 11:19:10 +0100

    worked

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    elments = []
-    for i in set_2:
-        if i in set_1:
-            continue
-        else:
-            elments.append(i)
-
-        for i in set_1:
-            if (i in set_2) or (i in elments):
-                continue
-            else:
-                elments.append(i)
-
-    return (elments)
+    return (set_1 ^ set_2)
