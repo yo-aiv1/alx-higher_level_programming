312bd2ffef3ff9984e7921a85843dfe833d6b41a
Yahya <yahyaoulha0@gmail.com>
2023-10-11 21:02:06 +0100

    fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     dir = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     nums = []
     result = 0
+    if roman_string == None:
+        return 0
 
     for i, char in enumerate(roman_string):
         if char in dir.keys():
