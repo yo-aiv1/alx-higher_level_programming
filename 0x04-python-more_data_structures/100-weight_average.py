6cc35962c4e84cbf9abaea5da225028b4f10abce
Yahya <yahyaoulha0@gmail.com>
2023-10-11 21:21:30 +0100

    fixed

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,9 @@
 def weight_average(my_list=[]):
     num = 0
     sub = 0
+    if len(my_list) == 0:
+        return (0)
     for i in my_list:
         num += (i[0] * i[1])
         sub += i[1]
-    return num / sub
+    return (num / sub)
