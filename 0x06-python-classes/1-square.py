05dada72f7445d8121f44ce400f849b14ef0b745
Yahya <yahyaoulha0@gmail.com>
2023-10-24 15:43:21 +0100

    worked

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A class representing a square with a given size.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialize a new square with the given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+    """
+    Define the instance variable "__size" to store the size of the square.
+    """
