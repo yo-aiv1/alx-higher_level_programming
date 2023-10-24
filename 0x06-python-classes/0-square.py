affab9db17b38fdc4b19b0fd4cdefd60d0240cff
Yahya <yahyaoulha0@gmail.com>
2023-10-24 15:27:48 +0100

    worked

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Square class.
+
+nothing
+"""
+
+
+class Square:
+    """
+    Square this class does not do anything.
+
+    Returns:
+        nothing
+    """
+
+    pass
