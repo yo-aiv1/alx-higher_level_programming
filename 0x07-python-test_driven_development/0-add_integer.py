8abe5d3403d0b833c61af30be43fd86c98052cac
Yahya <yahyaoulha0@gmail.com>
2023-10-27 12:30:08 +0100

    added docstring

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""summary."""
+
+
 def add_integer(a, b=98):
+    """Add 2 integers.
+
+    Args:
+        a (int): integer to add.
+        b (int): integer to add. Defaults to 98.
+    Returns:
+        int: result of adding a and b.
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
