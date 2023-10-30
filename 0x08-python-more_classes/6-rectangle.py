dccf3b205a3e31517415fef8dc02c0222b5510ae
Yahya <yahyaoulha0@gmail.com>
2023-10-30 19:03:57 +0100

    worked

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """A class representing a rectangle with a given size."""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialize a new rectangle.
 
