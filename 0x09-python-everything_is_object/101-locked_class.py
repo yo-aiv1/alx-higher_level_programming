e22266276bfeaf1570e29050303fc24d5ef2bb86
Yahya <yahyaoulha0@gmail.com>
2023-10-31 16:15:27 +0100

    worked

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Summary."""
+
+
+class LockedClass:
+    """LockedClass."""
+
+    __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method."""
+        pass
