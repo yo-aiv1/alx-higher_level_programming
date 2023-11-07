8e91d183333643358c0992dc0608397c619077de
Yahya <yahyaoulha0@gmail.com>
2023-11-07 11:21:40 +0100

    worked

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """Read files function.
+
+    Args:
+        filename (str): filename.
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        for i in f.readlines():
+            print(i, end='')
