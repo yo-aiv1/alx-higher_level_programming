3070275cb15a417f72109d913802be17b058e91c
Yahya <yahyaoulha0@gmail.com>
2023-11-07 17:17:49 +0100

    worked

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Summary."""
+
+
+def pascal_triangle(n):
+    """Print pascal triangle.
+
+    Args:
+        n (_type_): the size
+
+    Returns:
+        None
+    """
+    return None
