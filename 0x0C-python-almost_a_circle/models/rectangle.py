ed40c35e352a033a40a48c8c20346533ee8e5071
Yahya <yahyaoulha0@gmail.com>
2023-11-09 13:40:16 +0100

    worked

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,48 +8,64 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Instantiate method."""
-        self._width = width
-        self._height = height
-        self._x = x
+        self.width = width
+        self.height = height
+        self.x = x
         self.y = y
         super().__init__(id)
 
     @property
     def width(self):
         """Return width."""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """Set width."""
-        self._width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
     @property
     def height(self):
         """Return height."""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
         """Set height."""
-        self._height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
         """Return x."""
-        return self._x
+        return self.__x
 
     @x.setter
     def x(self, value):
         """Set x."""
-        self._x = value
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
         """Return y."""
-        return self._y
+        return self.__y
 
     @y.setter
     def y(self, value):
         """Set y."""
-        self._y = value
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
