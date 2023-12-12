b9efab2af21d66261f3b2be02fa87d33e73bfe61
Yahya <yahyaoulha0@gmail.com>
2023-12-12 15:23:24 +0100

    worked

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,9 @@
+-- create new table in the current databse
+-- and insert data into it
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT)
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, 'John', 10)
+    (2, 'Alex', 3)
+    (3, 'Bob', 14)
+    (4, 'George', 8)
