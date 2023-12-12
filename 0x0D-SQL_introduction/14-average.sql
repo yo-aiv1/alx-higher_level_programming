d86918cfd6cf380431e45896debce28e1c61dcd3
Yahya <yahyaoulha0@gmail.com>
2023-12-12 16:15:41 +0100

    worked

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- this script will compute and print the score averge
 -- of all records in the table second_table of the current databse
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
