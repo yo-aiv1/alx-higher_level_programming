4968b6d066d21487634f09078f0aa7508c928cb9
Yahya <yahyaoulha0@gmail.com>
2023-12-12 16:21:33 +0100

    worked

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- print the number of records with the same score in
+-- the table second_table of the current database
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
