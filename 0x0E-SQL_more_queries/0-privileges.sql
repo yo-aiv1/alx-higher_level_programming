381570548500f4e7cbcf5bd7447954914cbcd4ff
Yahya <yahyaoulha0@gmail.com>
2023-12-13 13:16:43 +0100

    fixed

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- this scirpt will show all privileges for the users
 -- user_0d_1 and user_0d_2 on the current server
-SHOW GRANT 'user_0d_1'@'localhost';
-SHOW GRANT 'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
