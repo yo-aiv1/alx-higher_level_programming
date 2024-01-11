fcbe23b404c66627f94d62e71bca2da512dda192
Yahya <yahyaoulha0@gmail.com>
2024-01-11 12:41:08 +0100

    worked

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Retreave data from a database."""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+                        host="localhost",
+                        user=sys.argv[1],
+                        password=sys.argv[2],
+                        db=sys.argv[3],
+                        port=3306
+                        )
+
+    cur = db.cursor()
+    arg = str(sys.argv[4])
+
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (arg,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
