67bbc2902dbccf66104c7708335d21730fa5537d
Yahya <yahyaoulha0@gmail.com>
2024-01-12 21:19:04 +0100

    worked

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Fetch the first row at the table states."""
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == '{}'.
+                                        format(sys.argv[4])).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
