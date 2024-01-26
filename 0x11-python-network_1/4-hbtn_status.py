d71c62b6065065b1bdfcd2e865a37f6247d05603
Yahya <yahyaoulha0@gmail.com>
2024-01-26 17:13:25 +0100

    worked

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Fetch and print response """
+import requests
+
+
+if __name__ == "__main__":
+	request = requests.get('https://alx-intranet.hbtn.io/status')
+	response = request.text
+	print('Body response:')
+	print('\t- type: {}'.format(type(response)))
+    print('\t- content: {}'.format(response))
