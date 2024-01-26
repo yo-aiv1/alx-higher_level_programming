24ccd29613dcd44574a88348a82f5c5e1e47f0ef
Yahya <yahyaoulha0@gmail.com>
2024-01-26 21:37:45 +0100

    worked

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+fetch the last 10 commits of a repository in github
+using Github API
+"""
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    apiurl = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    request = get(apiurl)
+    jsonobject = request.json()
+    for i in range(0, 10):
+        sha = jsonobject[i].get('sha')
+        name = jsonobject[i].get('commit').get('author').get('name')
+        print("{}: {}".format(sha, name))
