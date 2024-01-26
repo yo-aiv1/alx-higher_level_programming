71d2b36b06af320a79427192f377a4f4433a5b2c
Yahya <yahyaoulha0@gmail.com>
2024-01-26 21:14:29 +0100

    worked

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+send a POST request to an api with the letter as a parameter
+and print the response
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {'q': ""}
+    apiurl = "http://0.0.0.0:5000/search_user"
+
+    if len(sys.argv[1]) != 0:
+        data['q'] = sys.argv[1]
+ 
+    request = requests.post(apiurl, data)
+
+    try:
+        jsonobject = request.json()
+        if not jsonobject:
+            print("No result")
+        else:
+            print("[{}] ".format(jsonobject.get('id')), end='')
+            print("{}".format(jsonobject.get('name')))
+    except Exception:
+        print("Not a valid JSON")
