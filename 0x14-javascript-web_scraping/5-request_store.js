64702864624f3fad15b7a5fd433a99e1cfc7d74b
Yahya <yahyaoulha0@gmail.com>
2024-02-20 14:43:20 +0100

    worked

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
