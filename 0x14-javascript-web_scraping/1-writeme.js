9ab1ecf80e85af4d990be472197a6b15acc5226f
Yahya <yahyaoulha0@gmail.com>
2024-02-20 13:27:30 +0100

    worked

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-let data = process.argv[3]
+const data = process.argv[3];
 fs.writeFile(process.argv[2], data, 'utf8', function (err) {
   if (err) {
     console.log(err);
