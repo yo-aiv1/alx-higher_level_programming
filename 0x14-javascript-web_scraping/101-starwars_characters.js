e6c8533f765326b1a139a2b0d25e1cea997d81f8
Yahya <yahyaoulha0@gmail.com>
2024-02-20 20:54:47 +0100

    worked

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,27 +2,26 @@
 const request = require('request');
 
 const api = 'https://swapi-api.alx-tools.com/api/films/';
-arg = parseInt(process.argv[2])
+const arg = parseInt(process.argv[2]);
 const MovieId = function () {
-  if (arg != 0) {
+  if (arg !== 0) {
     return arg - 1;
   } else {
     return 0;
   }
-}
+};
 
 request(api, function (err, response, body) {
   if (err == null) {
-    const JsonObj = JSON.parse(body)
-    const cast = JsonObj.results[MovieId()].characters
+    const JsonObj = JSON.parse(body);
+    const cast = JsonObj.results[MovieId()].characters;
     for (let i = 0; i < cast.length; i++) {
       request(cast[i], function (err, response, body) {
         if (err == null) {
           const CharacterObj = JSON.parse(body);
-          console.log(CharacterObj.name)
+          console.log(CharacterObj.name);
         }
-      })
+      });
     }
-
   }
 });
